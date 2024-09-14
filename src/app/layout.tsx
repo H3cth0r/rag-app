ad12f8aad68d9d31a6b17ec610a8bc46221dd3ec
h3cth0r <hectorsucre13@gmail.com>
2024-09-13 23:38:50 -0600

    fixed the root background to adjust to selected theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,9 @@ export default function RootLayout({
         >
           <NavBar />
           {children}
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Taxing Laughter: The Joke Tax Chronicles
+    </h1>
         </ThemeProvider>
       </body>
     </html>
